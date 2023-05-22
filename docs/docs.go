c12dfb620b9a213dd628ed4ab9749ba727797f77
tk8320 <116806451+tk8320@users.noreply.github.com>
2023-05-22 17:58:51 +0530

    Update docs.go

--- docs/docs.go
+++ docs/docs.go
@@ -282,7 +282,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "2.0",
-	Host:             "https://go-fiber-app.onrender.com",
+	Host:             "go-fiber-app.onrender.com",
 	BasePath:         "/",
 	Schemes:          []string{"http"},
 	Title:            "Fiber Swagger Example API",

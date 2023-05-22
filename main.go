25df61fc8410b8a07533f64a6996aa682d23bdd2
tk8320 <116806451+tk8320@users.noreply.github.com>
2023-05-22 17:37:51 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	_ "go-fiber/docs"
 	"go-fiber/routes"
 	"log"
 )
 
+// @title Fiber Swagger Example API
+// @version 2.0
+// @description This is a sample server server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host https://go-fiber-app.onrender.com
+// @BasePath /
+// @schemes http
 func main() {
 
 	app := fiber.New()
